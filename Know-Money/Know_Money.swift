6499fce19a616cad2baee4a146e98fb006e9daab
Amankhan2370 <threeaura@gmail.com>
2024-12-18 23:16:28 -0800

    Initial commit

---
+++ Know-Money/Know_Money.swift
@@ -0,0 +1,40 @@
+
+import SwiftUI
+import CoreData
+
+@main
+struct Know_Money: App {
+    
+    init() {
+        self.setDefaultPreferences()
+    }
+    
+    private func setDefaultPreferences() {
+        let currency = UserDefaults.standard.string(forKey: UD_EXPENSE_CURRENCY)
+        if currency == nil {
+            UserDefaults.standard.set("₹", forKey: UD_EXPENSE_CURRENCY)
+        }
+    }
+    
+    var body: some Scene {
+        WindowGroup {
+            if UserDefaults.standard.bool(forKey: UD_USE_BIOMETRIC) {
+                AuthenticateView(viewModel: AuthenticationViewModel())
+                    .environment(\.managedObjectContext, persistentContainer.viewContext)
+            } else {
+                ExpenseView()
+                    .environment(\.managedObjectContext, persistentContainer.viewContext)
+            }
+        }
+    }
+    
+    var persistentContainer: NSPersistentContainer = {
+        let container = NSPersistentContainer(name: "Know_Money")
+        container.loadPersistentStores(completionHandler: { (storeDescription, error) in
+            if let error = error as NSError? {
+                fatalError("Unresolved error \(error), \(error.userInfo)")
+            }
+        })
+        return container
+    }()
+}
---
+++ Know-Money/Screens/ExpenseSettings/ExpenseCSVModel.swift
@@ -0,0 +1,12 @@
+
+
+import Foundation
+
+class ExpenseCSVModel: NSObject {
+    var title: String = ""
+    var amount: String = ""
+    var transactionType: String = ""
+    var tag: String = ""
+    var occuredOn: String = ""
+    var note: String = ""
+}
